5a1ff8b0883dff266983c63a8d9358d18538c6a5
preygoti <preygoti11@gmail.com>
2026-08-23 14:05:19 +0530

    23_08_2026

---
+++ 23_08_2026.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+#define SIZE 5
+
+int array[SIZE];
+
+int no,choice,pointer=-1;
+
+void PUSH(int no)
+{
+
+}
+void POP()
+{
+
+}
+void DISPLAY()
+{
+
+}
+int main()
+{
+
+    while(1)
+    {
+    printf("\n1.PUSH \n2.POP \n3.DISPLAY \n4.EXIT");
+
+    printf("\nEnter the choice :");
+    scanf("%d",&choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("Enter the number that you want to add in Stack :");
+        scanf("%d",&no);
+        PUSH(no);  
+    break;
+
+    case 2:
+            POP();
+    break;
+    case 3:
+            DISPLAY();
+    break;
+
+    case 4:
+            return 0;
+    }
+
+    }
+}
